9b715a6a3bd16fb8fbe5988831af59159f298320
Derek Huang <derek.huang@monash.edu>
2024-05-21 17:53:19 +1000

    Moved navbar layout to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
 import "@/styles/globals.css"
+import { Layout, Menu } from "antd"
 import type { AppProps } from "next/app"
 
+const { Header } = Layout
+
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+	return (
+		<Layout className="min-h-screen min-w-screen">
+			<Header>
+				<article className="text-primary">NICE Data Project</article>
+				<Menu
+					theme="dark"
+					mode="horizontal"
+					defaultSelectedKeys={["2"]}
+					items={[{ key: "1", label: "item1" }]}
+				/>
+			</Header>
+			<Component {...pageProps} />
+		</Layout>
+	)
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,86 +1,37 @@
 import React from "react"
-import {
-	LaptopOutlined,
-	NotificationOutlined,
-	UserOutlined,
-} from "@ant-design/icons"
-import type { MenuProps } from "antd"
-import { Breadcrumb, Layout, Menu, theme } from "antd"
+import { LaptopOutlined } from "@ant-design/icons"
+import { Layout, Menu } from "antd"
 
-const { Header, Content, Sider } = Layout
+const { Content, Sider } = Layout
 
-const items1: MenuProps["items"] = ["1", "2", "3"].map((key) => ({
-	key,
-	label: `nav ${key}`,
-}))
-
-const items2: MenuProps["items"] = [
-	UserOutlined,
-	LaptopOutlined,
-	NotificationOutlined,
-].map((icon, index) => {
-	const key = String(index + 1)
-
-	return {
-		key: `sub${key}`,
-		icon: React.createElement(icon),
-		label: `subnav ${key}`,
-
-		children: new Array(4).fill(null).map((_, j) => {
-			const subKey = index * 4 + j + 1
-			return {
-				key: subKey,
-				label: `option${subKey}`,
-			}
-		}),
-	}
-})
-
-const Home: React.FC = () => {
-	const {
-		token: { colorBgContainer, borderRadiusLG },
-	} = theme.useToken()
-
+function Home() {
 	return (
 		<Layout>
-			<Header style={{ display: "flex", alignItems: "center" }}>
-				<div className="demo-logo" />
+			<Sider width={200}>
 				<Menu
-					theme="dark"
-					mode="horizontal"
-					defaultSelectedKeys={["2"]}
-					items={items1}
-					style={{ flex: 1, minWidth: 0 }}
+					mode="inline"
+					defaultSelectedKeys={["1"]}
+					defaultOpenKeys={["sub1"]}
+					className="h-full"
+					items={[
+						{
+							key: "3",
+							label: "option 1",
+							icon: <LaptopOutlined />,
+							children: [
+								{
+									key: "2",
+									label: "suboption 1",
+								},
+							],
+						},
+					]}
 				/>
-			</Header>
-			<Layout>
-				<Sider width={200} style={{ background: colorBgContainer }}>
-					<Menu
-						mode="inline"
-						defaultSelectedKeys={["1"]}
-						defaultOpenKeys={["sub1"]}
-						style={{ height: "100%", borderRight: 0 }}
-						items={items2}
-					/>
-				</Sider>
-				<Layout style={{ padding: "0 24px 24px" }}>
-					<Breadcrumb style={{ margin: "16px 0" }}>
-						<Breadcrumb.Item>Home</Breadcrumb.Item>
-						<Breadcrumb.Item>List</Breadcrumb.Item>
-						<Breadcrumb.Item>App</Breadcrumb.Item>
-					</Breadcrumb>
-					<Content
-						style={{
-							padding: 24,
-							margin: 0,
-							minHeight: 280,
-							background: colorBgContainer,
-							borderRadius: borderRadiusLG,
-						}}
-					>
-						Content
-					</Content>
-				</Layout>
+			</Sider>
+			<Layout className="p-24">
+				<Content className="p-8 h-fit bg-slate-300">
+					<div className="h-[2000px]"></div>
+				</Content>
 			</Layout>
 		</Layout>
 	)
