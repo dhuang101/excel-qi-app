c62f93b0d80e31cc7de00bea5f56553de8c6a976
Derek Huang <derek.huang@monash.edu>
2024-05-21 14:36:55 +1000

    Adjusting document for antd with next

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,49 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import React from "react"
+import { createCache, extractStyle, StyleProvider } from "@ant-design/cssinjs"
+import Document, {
+	Head,
+	Html,
+	Main,
+	NextScript,
+	DocumentContext,
+} from "next/document"
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default function AppDocument() {
+	return (
+		<Html lang="en">
+			<Head />
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	)
+}
+
+// function is adjusted for Antd Component Package
+// https://ant.design/docs/react/use-with-next
+AppDocument.getInitialProps = async (ctx: DocumentContext) => {
+	const cache = createCache()
+	const originalRenderPage = ctx.renderPage
+	ctx.renderPage = () =>
+		originalRenderPage({
+			enhanceApp: (App) => (props) =>
+				(
+					<StyleProvider cache={cache}>
+						<App {...props} />
+					</StyleProvider>
+				),
+		})
+
+	const initialProps = await Document.getInitialProps(ctx)
+	const style = extractStyle(cache, true)
+	return {
+		...initialProps,
+		styles: (
+			<>
+				{initialProps.styles}
+				<style dangerouslySetInnerHTML={{ __html: style }} />
+			</>
+		),
+	}
 }
