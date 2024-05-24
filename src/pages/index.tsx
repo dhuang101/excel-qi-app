63f1d950413ec63ab8864ae7273422f731b47bbd
Derek Huang <derek.huang@monash.edu>
2024-05-24 15:08:22 +1000

    Added placeholder text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,30 @@
 function Home() {
-	return <div className="bg-base-100">Home Page</div>
+	return (
+		<div className="flex flex-col h-full w-2/3 items-center justify-center bg-base-100">
+			<article className="text-5xl font-semibold mb-8">
+				NICE Data Project
+			</article>
+			<article>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+				eiusmod tempor incididunt ut labore et dolore magna aliqua.
+				Aenean pharetra magna ac placerat vestibulum lectus mauris
+				ultrices. Dolor purus non enim praesent elementum facilisis. In
+				ante metus dictum at. Lorem dolor sed viverra ipsum nunc aliquet
+				bibendum enim facilisis. Magna sit amet purus gravida quis.
+				Mauris nunc congue nisi vitae suscipit tellus mauris a. Risus
+				pretium quam vulputate dignissim suspendisse in. Accumsan tortor
+				posuere ac ut consequat semper viverra nam libero. Etiam sit
+				amet nisl purus in mollis. Convallis a cras semper auctor neque
+				vitae tempus quam pellentesque. Sit amet nisl suscipit
+				adipiscing bibendum est ultricies integer. Luctus accumsan
+				tortor posuere ac. Facilisis sed odio morbi quis commodo odio.
+				Faucibus vitae aliquet nec ullamcorper sit amet. Adipiscing at
+				in tellus integer feugiat scelerisque varius. Aenean vel elit
+				scelerisque mauris pellentesque pulvinar pellentesque habitant.
+				Urna molestie at elementum eu facilisis sed odio morbi quis.
+			</article>
+		</div>
+	)
 }
 
 export default Home
