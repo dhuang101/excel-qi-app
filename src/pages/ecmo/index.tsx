0ebe827da891ae5ac46a6791f4a9b480432c05bc
Derek Huang <derek.huang@monash.edu>
2024-05-31 16:12:53 +1000

    Visualisation of input and output of ecmo model

--- src/pages/ecmo/index.tsx
+++ src/pages/ecmo/index.tsx
@@ -1,75 +1,170 @@
+import { testEcmo } from "@/test-data/ecmo"
 import axios from "axios"
-import React from "react"
-import { useState } from "react"
+import React, { useEffect, useState } from "react"
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined"
 
 function EcmoPage() {
-	const testVars = {
-		model_name: "Full",
-		variables: {
-			Year: 1,
-			AgeYears: 18,
-			BMI: 28,
-			"Coronary Artery Disease": 0,
-			"Chronic Heart Failure": 0,
-			"Chronic Lung Disease": 0,
-			"Pre ECMo C/T Surg": 0,
-			"PE ECLS Ltx": 0,
-			"Pulmonary Embolism": 0,
-			AKI: 0,
-			SBP: 77,
-			"Pre-ECMO Cardiac Arrest": 0,
-			ECPR: 0,
-			IntubationToTimeOnHours: 0,
-			RateBreathsSec: 12,
-			FiO2: 100,
-			"Renal Replacement Therapy": 0,
-			"Vasopressors/Inotropes": 0,
-			Bicarbonate: 0,
-			Lactate: 7.2,
-			pH: 7.27,
-			PO2: 150,
-			PCO2: 47,
-			HCO3: 19,
-		},
-	}
+	const testVars = testEcmo
 
-	const [prediction, setPrediction] = useState({})
+	const [modelDetails, setModelDetails]: any = useState({})
+	const [prediction, setPrediction] = useState({
+		out_value: null,
+		base_value: null,
+		altering_features: [],
+	})
 
 	function handleClick() {
 		axios.post("http://localhost:5000/evaluate", testVars).then((res) => {
-			console.log(res.data)
+			res.data.altering_features.sort(
+				(a: { value: number }, b: { value: number }) => {
+					return Math.abs(b.value) - Math.abs(a.value)
+				}
+			)
 			setPrediction(res.data)
 		})
 	}
 
+	function handleBack() {
+		setPrediction({
+			out_value: null,
+			base_value: null,
+			altering_features: [],
+		})
+	}
+
+	useEffect(() => {
+		axios.get("http://localhost:5000/models").then((res) => {
+			setModelDetails(
+				res.data.filter((obj: { name: string }) => {
+					return obj.name === "Full"
+				})[0]
+			)
+		})
+	}, [])
+
 	return (
-		<div className="flex flex-col h-full w-full justify-center items-center">
-			<article>Bruno Wilfred</article>
-			{Object.keys(prediction).length > 0 ? (
+		<div className="flex flex-col h-full w-full mt-8 items-center">
+			<article className="text-3xl font-semibold mb-4">
+				Bruno Wilfred
+			</article>
+			{prediction.base_value !== null && prediction.out_value !== null ? (
 				<React.Fragment>
-					<article>
+					<article className="text-3xl mb-4">
 						Survival to Discharge:{" "}
 						{(prediction.out_value * 100).toFixed(1)}%
 					</article>
-					<article>
-						Base Value: {(prediction.base_value * 100).toFixed(1)}%
+					<article className="text-xl mb-2">
+						Output Explanation
 					</article>
+					<article className="text-lg mb-2">
+						Starting Value:{" "}
+						{(prediction.base_value * 100).toFixed(1)}%
+					</article>
+					<div className="flex w-3/12 py-1 justify-between">
+						<article className="text-lg">Variable</article>
+						<article className="text-lg">Contribution</article>
+					</div>
+					{prediction.altering_features.map(
+						(obj: { label: string; value: number }) => {
+							return (
+								<div
+									key={obj.label}
+									className="flex w-3/12 border-b border-primary py-1 px-4 justify-between"
+								>
+									<article className="text-lg">
+										{obj.label}:{" "}
+									</article>
+									<article>
+										{(obj.value * 100).toFixed(1)}%
+									</article>
+								</div>
+							)
+						}
+					)}
+					<button
+						className="btn btn-primary mt-4"
+						onClick={handleBack}
+					>
+						Go Back
+					</button>
 				</React.Fragment>
-			) : (
+			) : Object.keys(modelDetails).length > 0 ? (
 				<React.Fragment>
-					{Object.entries(testVars.variables).map((obj) => {
-						return (
-							<div className="flex" key={obj[0]}>
-								<article>
-									{obj[0]}: {obj[1]}
-								</article>
-							</div>
-						)
-					})}
-					<button className="btn" onClick={handleClick}>
+					<article className="text-lg font-semibold">
+						Model: {modelDetails.name}
+					</article>
+					{modelDetails.categories.map(
+						(obj: {
+							name: string
+							features: {
+								name: string
+								label: string
+								description: string
+							}[]
+						}) => {
+							return (
+								<React.Fragment key={obj.name}>
+									<article
+										className="text-lg mt-4"
+										key={obj.name}
+									>
+										{obj.name}
+									</article>
+									{obj.features.map(
+										(innerObj: {
+											name: string
+											label: string
+											description: string
+										}) => {
+											if (innerObj.name === "Year") {
+												return
+											}
+											return (
+												<div
+													key={innerObj.name}
+													className="flex w-5/12 border-b border-primary py-1 px-4 justify-between"
+												>
+													<div
+														className="flex tooltip tooltip-left"
+														data-tip={
+															innerObj.description
+														}
+													>
+														<div className="mr-2">
+															<InfoOutlinedIcon />
+														</div>
+														<article>
+															{innerObj.label}
+														</article>
+													</div>
+													<article>
+														{testVars.variables[
+															innerObj.name
+														] === 0
+															? "False"
+															: testVars
+																	.variables[
+																	innerObj
+																		.name
+															  ]}
+													</article>
+												</div>
+											)
+										}
+									)}
+								</React.Fragment>
+							)
+						}
+					)}
+					<button
+						className="btn btn-primary mt-4"
+						onClick={handleClick}
+					>
 						Run Prediction
 					</button>
 				</React.Fragment>
+			) : (
+				<div>LOADING...</div>
 			)}
 		</div>
 	)
