6f42590245d023b31f853c6302dfab5472399d49
Derek Huang <derek.huang@monash.edu>
2024-08-01 15:30:24 +1000

    Update example fhir resources

--- src/test-data/fhir.ts
+++ src/test-data/fhir.ts
@@ -18,7 +18,7 @@ export const testFhirPatient = {
 export const testFhirObs = [
 	{
 		resourceType: "Observation",
-		id: "example-observation-1",
+		id: "observation-sbp",
 		status: "final",
 		category: [
 			{
@@ -53,7 +53,7 @@ export const testFhirObs = [
 	},
 	{
 		resourceType: "Observation",
-		id: "example-observation-2",
+		id: "observation-ph",
 		status: "final",
 		category: [
 			{
@@ -88,7 +88,7 @@ export const testFhirObs = [
 	},
 	{
 		resourceType: "Observation",
-		id: "example-observation-3",
+		id: "observation-rr",
 		status: "final",
 		category: [
 			{
@@ -105,7 +105,7 @@ export const testFhirObs = [
 			coding: [
 				{
 					system: "http://loinc.org",
-					code: "14743-9",
+					code: "9279-1",
 					display: "Respiratory rate",
 				},
 			],
@@ -121,12 +121,188 @@ export const testFhirObs = [
 			code: "/min",
 		},
 	},
+	{
+		resourceType: "Observation",
+		id: "observation-fio2",
+		status: "final",
+		category: [
+			{
+				coding: [
+					{
+						system: "http://terminology.hl7.org/CodeSystem/observation-category",
+						code: "vital-signs",
+						display: "Vital Signs",
+					},
+				],
+			},
+		],
+		code: {
+			coding: [
+				{
+					system: "http://loinc.org",
+					code: "3150-0",
+					display: "Inhaled oxygen concentration",
+				},
+			],
+		},
+		subject: {
+			reference: "Patient/example-patient",
+		},
+		effectiveDateTime: "2024-07-29T00:00:00Z",
+		valueQuantity: {
+			value: 100,
+			unit: "%",
+			system: "http://unitsofmeasure.org",
+			code: "%",
+		},
+	},
+	{
+		resourceType: "Observation",
+		id: "observation-lactate",
+		status: "final",
+		category: [
+			{
+				coding: [
+					{
+						system: "http://terminology.hl7.org/CodeSystem/observation-category",
+						code: "laboratory",
+						display: "Laboratory",
+					},
+				],
+			},
+		],
+		code: {
+			coding: [
+				{
+					system: "http://loinc.org",
+					code: "2524-7",
+					display: "Lactate [Moles/volume] in Blood",
+				},
+			],
+		},
+		subject: {
+			reference: "Patient/example-patient",
+		},
+		effectiveDateTime: "2024-07-29T00:00:00Z",
+		valueQuantity: {
+			value: 7.2,
+			unit: "mmol/L",
+			system: "http://unitsofmeasure.org",
+			code: "mmol/L",
+		},
+	},
+	{
+		resourceType: "Observation",
+		id: "observation-po2",
+		status: "final",
+		category: [
+			{
+				coding: [
+					{
+						system: "http://terminology.hl7.org/CodeSystem/observation-category",
+						code: "laboratory",
+						display: "Laboratory",
+					},
+				],
+			},
+		],
+		code: {
+			coding: [
+				{
+					system: "http://loinc.org",
+					code: "2703-7",
+					display: "Oxygen [Partial pressure] in Arterial blood",
+				},
+			],
+		},
+		subject: {
+			reference: "Patient/example-patient",
+		},
+		effectiveDateTime: "2024-07-29T00:00:00Z",
+		valueQuantity: {
+			value: 150,
+			unit: "mmHg",
+			system: "http://unitsofmeasure.org",
+			code: "mm[Hg]",
+		},
+	},
+	{
+		resourceType: "Observation",
+		id: "observation-pco2",
+		status: "final",
+		category: [
+			{
+				coding: [
+					{
+						system: "http://terminology.hl7.org/CodeSystem/observation-category",
+						code: "laboratory",
+						display: "Laboratory",
+					},
+				],
+			},
+		],
+		code: {
+			coding: [
+				{
+					system: "http://loinc.org",
+					code: "2019-8",
+					display:
+						"Carbon dioxide [Partial pressure] in Arterial blood",
+				},
+			],
+		},
+		subject: {
+			reference: "Patient/example-patient",
+		},
+		effectiveDateTime: "2024-07-29T00:00:00Z",
+		valueQuantity: {
+			value: 47,
+			unit: "mmHg",
+			system: "http://unitsofmeasure.org",
+			code: "mm[Hg]",
+		},
+	},
+	{
+		resourceType: "Observation",
+		id: "observation-hco3",
+		status: "final",
+		category: [
+			{
+				coding: [
+					{
+						system: "http://terminology.hl7.org/CodeSystem/observation-category",
+						code: "laboratory",
+						display: "Laboratory",
+					},
+				],
+			},
+		],
+		code: {
+			coding: [
+				{
+					system: "http://loinc.org",
+					code: "1963-8",
+					display: "Bicarbonate [Moles/volume] in Blood",
+				},
+			],
+		},
+		subject: {
+			reference: "Patient/example-patient",
+		},
+		effectiveDateTime: "2024-07-29T00:00:00Z",
+		valueQuantity: {
+			value: 19,
+			unit: "mmol/L",
+			system: "http://unitsofmeasure.org",
+			code: "mmol/L",
+		},
+	},
 ]
 
 export const testFhirCond = [
 	{
 		resourceType: "Condition",
-		id: "example-condition-1",
+		id: "condition-cad",
 		subject: {
 			reference: "Patient/example-patient",
 		},
@@ -143,8 +319,108 @@ export const testFhirCond = [
 			coding: [
 				{
 					system: "http://terminology.hl7.org/CodeSystem/condition-ver-status",
-					code: "refuted",
-					display: "Refuted",
+					code: "confirmed",
+					display: "Confirmed",
+				},
+			],
+		},
+	},
+	{
+		resourceType: "Condition",
+		id: "condition-chf",
+		subject: {
+			reference: "Patient/example-patient",
+		},
+		code: {
+			coding: [
+				{
+					system: "http://snomed.info/sct",
+					code: "10057000",
+					display: "Chronic Heart Failure",
+				},
+			],
+		},
+		verificationStatus: {
+			coding: [
+				{
+					system: "http://terminology.hl7.org/CodeSystem/condition-ver-status",
+					code: "confirmed",
+					display: "Confirmed",
+				},
+			],
+		},
+	},
+	{
+		resourceType: "Condition",
+		id: "condition-cld",
+		subject: {
+			reference: "Patient/example-patient",
+		},
+		code: {
+			coding: [
+				{
+					system: "http://snomed.info/sct",
+					code: "91488009",
+					display: "Chronic Lung Disease",
+				},
+			],
+		},
+		verificationStatus: {
+			coding: [
+				{
+					system: "http://terminology.hl7.org/CodeSystem/condition-ver-status",
+					code: "confirmed",
+					display: "Confirmed",
+				},
+			],
+		},
+	},
+	{
+		resourceType: "Condition",
+		id: "condition-pe",
+		subject: {
+			reference: "Patient/example-patient",
+		},
+		code: {
+			coding: [
+				{
+					system: "http://snomed.info/sct",
+					code: "233935004",
+					display: "Pulmonary Embolism",
+				},
+			],
+		},
+		verificationStatus: {
+			coding: [
+				{
+					system: "http://terminology.hl7.org/CodeSystem/condition-ver-status",
+					code: "confirmed",
+					display: "Confirmed",
+				},
+			],
+		},
+	},
+	{
+		resourceType: "Condition",
+		id: "condition-aki",
+		subject: {
+			reference: "Patient/example-patient",
+		},
+		code: {
+			coding: [
+				{
+					system: "http://snomed.info/sct",
+					code: "13960000",
+					display: "Acute Kidney Injury",
+				},
+			],
+		},
+		verificationStatus: {
+			coding: [
+				{
+					system: "http://terminology.hl7.org/CodeSystem/condition-ver-status",
+					code: "confirmed",
+					display: "Confirmed",
 				},
 			],
 		},
@@ -154,7 +430,7 @@ export const testFhirCond = [
 export const testFhirProc = [
 	{
 		resourceType: "Procedure",
-		id: "example-procedure-1",
+		id: "procedure-ecpr",
 		subject: {
 			reference: "Patient/example-patient",
 		},
@@ -162,14 +438,43 @@ export const testFhirProc = [
 			coding: [
 				{
 					system: "http://snomed.info/sct",
-					code: "52734007",
-					display: "Cardiopulmonary resuscitation",
+					code: "52870002",
+					display: "Extracorporeal Cardiopulmonary Resuscitation",
 				},
 			],
 		},
 		performedPeriod: {
 			start: "2024-07-29T00:00:00Z",
 			end: "2024-07-29T00:30:00Z",
+		},
+		outcome: {
+			coding: [
+				{
+					system: "http://terminology.hl7.org/CodeSystem/procedure-outcome",
+					code: "success",
+					display: "Successful",
+				},
+			],
+		},
+	},
+	{
+		resourceType: "Procedure",
+		id: "procedure-rtt",
+		subject: {
+			reference: "Patient/example-patient",
+		},
+		code: {
+			coding: [
+				{
+					system: "http://snomed.info/sct",
+					code: "108241001",
+					display: "Renal Replacement Therapy",
+				},
+			],
+		},
+		performedPeriod: {
+			start: "2024-07-29T00:00:00Z",
+			end: "2024-07-29T01:00:00Z",
 		},
 		outcome: {
 			coding: [
