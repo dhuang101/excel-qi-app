0b06258a83045f9e16a54a400441d4c8c98930d2
Derek Huang <derek.huang@monash.edu>
2024-09-23 15:31:08 +1000

    Constructed api for fetching from mongodb

---
+++ src/pages/api/database/getPatients.ts
@@ -0,0 +1,30 @@
+import { FindOptions, MongoClient } from "mongodb"
+
+async function GetPatients(params: any) {
+	// connect to db
+	const client = new MongoClient(process.env.DB_CONNECTION_URI as string)
+	const collection = client.db("main").collection("collection")
+	// options for find
+	const query = { record_id: { $regex: params.searchInput, $options: "i" } }
+	const options = {
+		// sort ascending
+		sort: { record_id: 1 },
+		// Include only the particular fields
+		projection: { _id: 0, record_id: 1 },
+	} as FindOptions
+	// run find
+	const results = await collection.find(query, options).toArray()
+	return results
+}
+
+// handler for any calls to this endpoint
+export default async function handler(req: any, res: any) {
+	const params = req.query
+
+	try {
+		const results = await GetPatients(params)
+		res.status(200).json(results)
+	} catch (err) {
+		res.status(500).json(err)
+	}
+}
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,14 +1,38 @@
-import { ChangeEvent, useState } from "react"
-import SearchTable from "../../components/excel/SearchTable"
+import { ChangeEvent, KeyboardEvent, useState } from "react"
+import SearchTable from "../../components/search/SearchTable"
 import { testPatients } from "@/test-data/patients"
+import axios from "axios"
 
 function SearchPage() {
 	const [searchOption, setSearchOption] = useState("name")
+	const [searchInput, setSearchInput] = useState("")
 	const [patientData, setPatientData] = useState(testPatients)
 
-	// function that handles input in the search box
+	// function that handles change in search option
 	function handleSelect(event: ChangeEvent<HTMLSelectElement>): void {
 		setSearchOption(event.target.value)
+	}
+	// function that handles input in the search box
+	function handleInput(event: ChangeEvent<HTMLInputElement>) {
+		setSearchInput(event.target.value)
+	}
+	// searches on enter press
+	function handleKeyDown(event: KeyboardEvent<HTMLInputElement>) {
+		if (event.key === "Enter") {
+			handleSearch()
+		}
+	}
+
+	function handleSearch() {
+		axios
+			.get("/api/database/getPatients", {
+				params: {
+					searchInput: searchInput,
+				},
+			})
+			.then((result) => {
+				console.log(result.data)
+			})
 	}
 
 	return (
@@ -31,8 +55,15 @@ function SearchPage() {
 					type="text"
 					placeholder="Enter Query"
 					className="input input-bordered w-full max-w-sm"
+					onKeyDown={handleKeyDown}
+					onChange={handleInput}
 				/>
-				<button className="ml-2 btn rounded btn-primary">Search</button>
+				<button
+					onClick={handleSearch}
+					className="ml-2 btn rounded btn-primary"
+				>
+					Search
+				</button>
 			</div>
 			<div className="mt-2">
 				<SearchTable patientData={patientData} />
