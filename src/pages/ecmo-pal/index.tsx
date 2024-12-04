def9c0a9dae22450aaf42163d11a3bc6cf9cd81a
Derek Huang <derek.huang@monash.edu>
2024-12-04 13:09:29 +1100

    adjust loading layout

--- src/pages/ecmo-pal/index.tsx
+++ src/pages/ecmo-pal/index.tsx
@@ -176,7 +176,7 @@ function EcmoPalPage() {
 						</button>
 					</React.Fragment>
 				) : (
-					<div className="flex justify-center items-center h-[90%]">
+					<div className="flex justify-center items-center h-[83vh]">
 						<CircularProgress size={80} />
 						<article className="text-lg font-semibold pt-4">
 							Fetching Models...
