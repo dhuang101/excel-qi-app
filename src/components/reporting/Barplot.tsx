2b1c8c55cd50227d745e5bcb9b9902ff490e24f1
Derek Huang <derek.huang@monash.edu>
2025-01-07 17:44:00 +1100

    Export wrap text function

--- src/components/reporting/Barplot.tsx
+++ src/components/reporting/Barplot.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from "react"
 import * as d3 from "d3"
+import { SvgWrapText } from "@/utilities/SvgWrapText"
 
 const MARGIN = { top: 30, right: 30, bottom: 30, left: 140 }
 const BAR_PADDING = 0.3
@@ -35,56 +36,6 @@ export const Barplot = ({ width, height, data }: BarplotProps) => {
 	}, [data, width])
 
 	// Build the shapes
-	const wrapText = (
-		textElement: d3.Selection<SVGTextElement, any, null, undefined>,
-		width: number
-	): void => {
-		const text = textElement
-		const words = text.text().split(/\s+/) // Split the text into words
-		let word
-		const line: string[] = []
-		const lineHeight = 1.1 // Line height multiplier
-		const x = text.attr("x")
-		const y = text.attr("y")
-		let dy = parseFloat(text.attr("dy") || "0")
-
-		// Only wrap text if it hasn't been wrapped already
-		if (text.selectAll("tspan").size() > 0) return // Exit if wrapping has already been done
-
-		// Start with the first tspan element
-		let tspan = text
-			.text(null)
-			.append("tspan")
-			.attr("x", x)
-			.attr("y", y)
-			.attr("dy", dy + "em")
-
-		while ((word = words.shift())) {
-			// Shift words from the front of the array
-			line.push(word)
-			tspan.text(line.join(" "))
-
-			// Safely check the computed text length
-			const computedTextLength =
-				tspan.node()?.getComputedTextLength() ?? 0
-
-			// Check if the current tspan width exceeds the allowed width
-			if (computedTextLength > width) {
-				// If it does, remove the last word and start a new tspan line
-				line.pop()
-				tspan.text(line.join(" "))
-				line.length = 0
-				line.push(word) // Push the current word to start a new line
-				tspan = text
-					.append("tspan")
-					.attr("x", x)
-					.attr("y", y)
-					.attr("dy", ++dy * lineHeight + "em")
-					.text(word)
-			}
-		}
-	}
-
 	const allShapes = data.map((d, i) => {
 		const y = yScale(d._id)
 		if (y === undefined) {
@@ -127,7 +78,7 @@ export const Barplot = ({ width, height, data }: BarplotProps) => {
 						// Use the wrapText function to wrap the text
 						if (node) {
 							const textElement = d3.select(node)
-							wrapText(textElement, xScale(0) + 128) // Wrap the text to fit within the available width
+							SvgWrapText(textElement, xScale(0) + 128) // Wrap the text to fit within the available width
 						}
 					}}
 				>
