a73b23eac3f3ca82920654a6a5ced8b2882034f5
Derek Huang <derek.huang@monash.edu>
2025-03-05 15:41:23 +1100

    Added logos for resources page

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,5 +1,12 @@
 const ASSETS = {
 	logo: new URL("./logo.png", import.meta.url).href,
+	alfred: new URL("./alfred.png", import.meta.url).href,
+	ccm: new URL("./ccm.png", import.meta.url).href,
+	elso: new URL("./elso.png", import.meta.url).href,
+	em: new URL("./em.png", import.meta.url).href,
+	jhm: new URL("./jhm.png", import.meta.url).href,
+	litfl: new URL("./litfl.jpg", import.meta.url).href,
+	vecmos: new URL("./vecmos.png", import.meta.url).href,
 }
 
 export default ASSETS
--- src/pages/resources/index.tsx
+++ src/pages/resources/index.tsx
@@ -1,5 +1,49 @@
 function ResourcesPage() {
-	return <div className="flex flex-col">resources pages</div>
+	return (
+		<div className="flex flex-col items-center">
+			<article className="my-4 text-3xl font-semibold">
+				ECMO Resources
+			</article>
+			<div className="flex gap-x-20">
+				<div className="card bg-base-100 w-96 shadow-sm">
+					<div>
+						<img
+							src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+							alt="Shoes"
+						/>
+					</div>
+					<div className="card-body bg-base-300">
+						<h2 className="card-title">Card Title</h2>
+						<p>
+							A card component has a figure, a body part, and
+							inside body there are title and actions parts
+						</p>
+						<div className="card-actions justify-end">
+							<button className="btn btn-primary">Buy Now</button>
+						</div>
+					</div>
+				</div>
+				<div className="card bg-base-100 w-96 shadow-sm">
+					<figure>
+						<img
+							src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+							alt="Shoes"
+						/>
+					</figure>
+					<div className="card-body bg-base-300">
+						<h2 className="card-title">Card Title</h2>
+						<p>
+							A card component has a figure, a body part, and
+							inside body there are title and actions parts
+						</p>
+						<div className="card-actions justify-end">
+							<button className="btn btn-primary">Buy Now</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default ResourcesPage
