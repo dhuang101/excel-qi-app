a91f41193632ed773ed96e014e3b63c463bf47e7
dhuang101 <82797171+dhuang101@users.noreply.github.com>
2025-04-03 15:16:19 +1100

    Only allow access to landing page without sign in

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,18 @@
-export { default } from "next-auth/middleware"
+import { withAuth } from "next-auth/middleware"
+import { NextResponse } from "next/server"
+
+export default withAuth(
+	function middleware(req) {
+		return NextResponse.next()
+	},
+	{
+		pages: {
+			signIn: "api/auth/signin", // Redirect unauthenticated users to this page
+		},
+	}
+)
+
+// Protect all pages EXCEPT the root ("/")
+export const config = {
+	matcher: ["/((?!$).*)"], // This ensures "/" is excluded from authentication
+}
