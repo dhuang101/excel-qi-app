694f4d117cec92ad9e6e2a3a181877783696ceef
Derek Huang <derek.huang@monash.edu>
2025-04-07 16:14:23 +1000

    fetch role from mongodb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,12 @@
+import { MongoClient } from "mongodb"
 import NextAuth from "next-auth"
 import Auth0Provider from "next-auth/providers/auth0"
+
+const client = await MongoClient.connect(
+	process.env.DB_CONNECTION_URI as string
+)
+const db = client.db("main")
+const rolesCollection = db.collection("permissions")
 
 export const authOptions = {
 	// Configure one or more authentication providers
@@ -10,6 +17,21 @@ export const authOptions = {
 			issuer: process.env.NEXT_PUBLIC_AUTH0_ISSUER,
 		}),
 	],
+	callbacks: {
+		async jwt({ token, user }: any) {
+			if (user?.email) {
+				const userRole = await rolesCollection.findOne({
+					email: user.email,
+				})
+				token.role = userRole?.role || "guest"
+			}
+			return token
+		},
+		async session({ session, token }: any) {
+			session.user.role = token.role
+			return session
+		},
+	},
 }
 
 export default NextAuth(authOptions)
