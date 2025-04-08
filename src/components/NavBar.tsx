6fe4e1babfe89fc08d35568e4c3ec5391b154f8a
Derek Huang <derek.huang@monash.edu>
2025-04-08 15:11:18 +1000

    Conditional button for admin on navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -64,11 +64,13 @@ function NavBar() {
 								ECMO Resources
 							</article>
 						</Link>
-						<Link href={"/admin"}>
-							<article className="btn btn-ghost normal-case rounded-xl text-md text-base-content">
-								Admin
-							</article>
-						</Link>
+						{session?.user.role === "admin" && (
+							<Link href={"/admin"}>
+								<article className="btn btn-ghost normal-case rounded-xl text-md text-base-content">
+									Admin
+								</article>
+							</Link>
+						)}
 					</div>
 				</div>
 				<div className="btn btn-ghost rounded-xl" onClick={toggleTheme}>
