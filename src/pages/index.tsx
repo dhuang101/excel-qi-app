366b5f8c3736aad3b483c6b1e5920edd9ca05ad1
dhuang101 <82797171+dhuang101@users.noreply.github.com>
2025-05-29 16:34:25 +1000

    Refactor Home component to replace placeholder text with project description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,26 +9,20 @@ function Home() {
 				<article className="text-5xl font-semibold mb-8">
 					NICE Data Project
 				</article>
-				<article>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-					do eiusmod tempor incididunt ut labore et dolore magna
-					aliqua. Aenean pharetra magna ac placerat vestibulum lectus
-					mauris ultrices. Dolor purus non enim praesent elementum
-					facilisis. In ante metus dictum at. Lorem dolor sed viverra
-					ipsum nunc aliquet bibendum enim facilisis. Magna sit amet
-					purus gravida quis. Mauris nunc congue nisi vitae suscipit
-					tellus mauris a. Risus pretium quam vulputate dignissim
-					suspendisse in. Accumsan tortor posuere ac ut consequat
-					semper viverra nam libero. Etiam sit amet nisl purus in
-					mollis. Convallis a cras semper auctor neque vitae tempus
-					quam pellentesque. Sit amet nisl suscipit adipiscing
-					bibendum est ultricies integer. Luctus accumsan tortor
-					posuere ac. Facilisis sed odio morbi quis commodo odio.
-					Faucibus vitae aliquet nec ullamcorper sit amet. Adipiscing
-					at in tellus integer feugiat scelerisque varius. Aenean vel
-					elit scelerisque mauris pellentesque pulvinar pellentesque
-					habitant. Urna molestie at elementum eu facilisis sed odio
-					morbi quis.
+				<article className="w-2/3">
+					The NICE Data Project is an innovative in development
+					project to provide an intelligent platform for users of the
+					EXCEL Registry by allowing them to efficiently explore and
+					visualize data. The project aims to enhance the
+					accessibility and usability of the data, making it easier
+					for researchers and healthcare professionals to derive
+					insights and make informed decisions.
+					<br />
+					<br />
+					The platform will also leverage AI-driven analysis to
+					interpret patient conditions and support recovery strategies
+					bringing advanced intelligence to the forefront of patient
+					care.
 				</article>
 			</div>
 		</div>
