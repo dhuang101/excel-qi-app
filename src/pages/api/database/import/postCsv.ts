11a2355c440c1e740fa5b22b8388d4f13b4ff9df
Derek Huang <derek.huang@monash.edu>
2025-07-23 09:23:45 +1000

    Remove console log from error handling in postCsv API handler

--- src/pages/api/database/import/postCsv.ts
+++ src/pages/api/database/import/postCsv.ts
@@ -21,7 +21,6 @@ export default async function handler(req: any, res: any) {
 		const results = await postCsv(params)
 		res.status(200).json(results)
 	} catch (err) {
-		console.log(err)
 		res.status(500).json(err)
 	}
 }
