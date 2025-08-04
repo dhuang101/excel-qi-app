29e829eb91a83db439faf7758f61d53616453ee7
dhuang101 <82797171+dhuang101@users.noreply.github.com>
2025-08-04 16:12:37 +1000

    Increase table header font size

--- src/components/baseComponents/Table.tsx
+++ src/components/baseComponents/Table.tsx
@@ -18,7 +18,7 @@ function Table<T>({ data, headers, renderRow, emptyMessage }: TableProps<T>) {
 								{headers.map((header, i) => (
 									<th
 										key={i}
-										className={`bg-base-300 ${
+										className={`bg-base-300 text-lg font-semibold ${
 											i === 0 ? "sticky left-0" : ""
 										}`}
 									>
