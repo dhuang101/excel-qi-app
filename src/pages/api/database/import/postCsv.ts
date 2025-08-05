e36c98832ffe75c3625b66861cf2e4074b68471e
Derek Huang <derek.huang@monash.edu>
2025-08-05 16:57:20 +1000

    Fixed import function importing data with incorrect format

--- src/pages/api/database/import/postCsv.ts
+++ src/pages/api/database/import/postCsv.ts
@@ -4,18 +4,55 @@ import { excelImportRow } from "@/types/excelImportTypes"
 // this api takes the translated and merged rows from the CSV import
 // and inserts them into the database
 
+const dateFields = [
+	"hospadm_date_time",
+	"icuadm_date_time",
+	"ecmo_start_date_time",
+	"decan_date_time",
+	"outcm_icu_discharge",
+	"outcm_hosp_discharge",
+]
+
+// Helper to convert string numbers to numbers, except for date fields
+function preprocessRow(row: excelImportRow): excelImportRow {
+	const returnVal: Record<string, any> = { ...row }
+	for (const key in returnVal) {
+		const value = returnVal[key]
+		if (
+			!dateFields.includes(key) &&
+			typeof value === "string" &&
+			!isNaN(Number(value)) &&
+			value.trim() !== ""
+		) {
+			returnVal[key] = Number(value)
+		} else if (dateFields.includes(key) && value) {
+			const splitDate = returnVal[key].split("T")[0].split("-")
+			returnVal[key] = new Date(
+				Date.UTC(
+					parseInt(splitDate[0]),
+					parseInt(splitDate[1]) - 1,
+					parseInt(splitDate[2])
+				)
+			)
+		}
+	}
+	return returnVal as excelImportRow
+}
+
 async function postCsv(params: excelImportRow[]) {
 	const client = new MongoClient(process.env.DB_CONNECTION_URI as string)
 	const collection = client
 		.db("main")
 		.collection<excelImportRow>("collection")
-	await collection.insertMany(params)
-	return { message: "Records inserted successfully" }
+	const normalizedParams: excelImportRow[] = params.map(preprocessRow)
+	console.log(normalizedParams)
+	await collection.insertMany(normalizedParams)
 }
 
 // handler for any calls to this endpoint
 export default async function handler(req: any, res: any) {
 	const params = req.body.records
+	console.log(params)
 
 	try {
 		const results = await postCsv(params)
