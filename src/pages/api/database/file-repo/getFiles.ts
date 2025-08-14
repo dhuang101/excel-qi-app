c72ad882eab664373d9b88fff0df6a7a22d5817b
dhuang101 <82797171+dhuang101@users.noreply.github.com>
2025-08-14 16:30:50 +1000

    skeleton for file repo

--- src/pages/api/database/file-repo/getFiles.ts
+++ src/pages/api/database/file-repo/getFiles.ts
@@ -1,13 +1,43 @@
-function GetFiles(params: any) {}
+import { MongoClient } from "mongodb"
+
+type ParamsType = {
+	role: string
+	sites: string[]
+}
+
+async function GetFiles(params: ParamsType) {
+	const client = new MongoClient(process.env.DB_CONNECTION_URI as string)
+	try {
+		await client.connect()
+		const collection = client.db("main").collection("file-repository")
+
+		let sites: string[] = params.sites
+		if (
+			(!sites || sites.length === 0) &&
+			(params.role === "admin" || params.role === "global-viewer")
+		) {
+			sites = await collection.distinct("redcap_data_access_group")
+		}
+
+		const query = { site: { $in: sites } }
+		const results = await collection
+			.find(query, { projection: { _id: 0 } })
+			.toArray()
+
+		return results
+	} finally {
+		await client.close()
+	}
+}
 
 // handler for any calls to this endpoint
 export default async function handler(req: any, res: any) {
-	const params = req.body.records
+	const params = req.body as ParamsType
 
 	try {
 		const results = await GetFiles(params)
 		res.status(200).json(results)
 	} catch (err) {
-		res.status(500).json(err)
+		res.status(500).json({ error: "Internal Server Error" })
 	}
 }
--- src/pages/file-repo/index.tsx
+++ src/pages/file-repo/index.tsx
@@ -1,18 +1,63 @@
+import { FormatSiteName } from "@/utilities/FormatSiteName"
 import axios from "axios"
-import { useEffect } from "react"
+import { useSession } from "next-auth/react"
+import { useEffect, useState } from "react"
+
+type FilesType = {
+	site: string
+	files: { name: string; link: string }[]
+}
 
 function FileRepoPage() {
+	// hooks
+	const { data: session, status } = useSession()
+	// state
+	const [files, setFiles] = useState<FilesType[]>([])
+
 	useEffect(() => {
-		axios.get("/api/database/file-repo/getFiles").then((result) => {
-			console.log(result)
-		})
-	}, [])
+		if (status !== "authenticated") {
+			return
+		}
+		axios
+			.post("/api/database/file-repo/getFiles", {
+				role: session?.user.role,
+				sites: session?.user.sites,
+			})
+			.then((result) => {
+				setFiles(result.data)
+				console.log(result.data)
+			})
+	}, [status])
 
 	return (
 		<div className="flex flex-col grow w-full items-center">
-			<article className="font-semibold mt-4 text-3xl">
-				File Repository
-			</article>
+			<div className="flex flex-col w-2/3 h-full items-center">
+				<article className="font-semibold mt-4 text-3xl">
+					File Repository
+				</article>
+				{files.map((site) => {
+					return (
+						<div className="mt-4 flex flex-col w-full items-center">
+							<article
+								className="font-semibold text-lg"
+								key={site.site}
+							>
+								{FormatSiteName(site.site)}
+							</article>
+							<div className="w-full mt-2">
+								{site.files.map((file) => {
+									return (
+										<div className="flex w-full justify-around ">
+											<article>{file.name}</article>
+											<article>{file.link}</article>
+										</div>
+									)
+								})}
+							</div>
+						</div>
+					)
+				})}
+			</div>
 		</div>
 	)
 }
