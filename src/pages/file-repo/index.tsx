b8a4f54e1e43e7e7856f3e6be9ece4a611d6e6f7
dhuang101 <82797171+dhuang101@users.noreply.github.com>
2025-08-28 16:58:13 +1000

    Starting modal for file upload

--- src/pages/file-repo/index.tsx
+++ src/pages/file-repo/index.tsx
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { useSession } from "next-auth/react"
+import React, { useRef } from "react"
 import { useEffect, useState } from "react"
 
 type FilesType = {
@@ -8,11 +9,19 @@ type FilesType = {
 	path: string
 }
 
+type ModalStatus = "selecting" | "confirming" | "updating" | "submitted"
+
 function FileRepoPage() {
 	// hooks
 	const { data: session, status } = useSession()
 	// state
 	const [files, setFiles] = useState<FilesType[]>([])
+	// modal state
+	const [modalStatus, setModalStatus] = useState<ModalStatus>("selecting")
+	const [modalKey, setModalKey] = useState(0)
+	const [error, setError] = useState(false)
+	const [file, setFile] = useState<File | null>(null)
+	const modalRef = useRef<HTMLDialogElement>(null)
 
 	useEffect(() => {
 		if (status !== "authenticated") {
@@ -25,7 +34,6 @@ function FileRepoPage() {
 			})
 			.then((result) => {
 				setFiles(result.data)
-				console.log(result.data)
 			})
 	}, [status])
 
@@ -55,31 +63,91 @@ function FileRepoPage() {
 	}
 
 	return (
-		<div className="flex flex-col grow w-full items-center">
-			<div className="flex flex-col w-2/3 h-full items-center">
-				<article className="font-semibold mt-4 text-3xl">
-					File Repository
-				</article>
-				{files.map((file) => {
-					console.log(file)
-					return (
-						<div
-							key={file.filename}
-							className="mt-4 flex flex-col w-full"
-						>
-							<article
+		<React.Fragment>
+			<div className="flex flex-col grow w-full items-center">
+				<div className="flex flex-col w-1/2 h-full items-center">
+					<article className="font-semibold mt-4 text-3xl">
+						File Repository
+					</article>
+					{session?.user.role !== "site-viewer" && (
+						<div className="flex w-full mt-4">
+							<button
 								onClick={() => {
-									downloadFile(file)
+									modalRef.current!.showModal()
 								}}
-								className="font-semibold text-lg underline hover:text-primary hover:no-underline cursor-pointer"
+								className="ml-auto btn btn-primary"
 							>
-								{file.filename}
-							</article>
+								Upload
+							</button>
 						</div>
-					)
-				})}
+					)}
+					{files.map((file) => {
+						return (
+							<div
+								key={file.filename}
+								className="mt-4 flex flex-col w-full"
+							>
+								<article
+									onClick={() => {
+										downloadFile(file)
+									}}
+									className="font-semibold text-lg underline ml-4 hover:text-primary hover:no-underline cursor-pointer"
+								>
+									{file.filename}
+								</article>
+								<div className="divider" />
+							</div>
+						)
+					})}
+				</div>
 			</div>
-		</div>
+			{/* Modal */}
+			<dialog
+				ref={modalRef}
+				className="modal"
+				onTransitionEnd={(event: React.TransitionEvent) => {
+					if (
+						!modalRef.current?.open &&
+						event.propertyName === "visibility" &&
+						modalStatus === "submitted"
+					) {
+						window.location.reload()
+					} else if (
+						!modalRef.current?.open &&
+						event.propertyName === "visibility"
+					) {
+						setError(false)
+						setModalStatus("selecting")
+						setModalKey((prev) => prev + 1)
+					}
+				}}
+			>
+				<div className="modal-box w-fit p-8">
+					<article className="font-semibold text-lg">
+						Upload To Repository
+					</article>
+					<fieldset className="fieldset mt-4">
+						<legend className="fieldset-legend">
+							.pdf file only
+						</legend>
+						<input
+							type="file"
+							accept=".pdf"
+							onChange={(e) =>
+								setFile(
+									e.target.files ? e.target.files[0] : null
+								)
+							}
+							className="file-input file-input-primary"
+						/>
+					</fieldset>
+					<button className="btn btn-primary mt-4">Preview</button>
+				</div>
+				<form method="dialog" className="modal-backdrop">
+					<button>close</button>
+				</form>
+			</dialog>
+		</React.Fragment>
 	)
 }
 
