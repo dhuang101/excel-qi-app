e5a4a8efe4cbc61cd81cabe3e150829f517af11c
Derek Huang <derek.huang@monash.edu>
2025-11-11 16:26:23 +1100

    Commenting

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,8 @@
 import { MongoClient } from "mongodb"
 import NextAuth, { User } from "next-auth"
 import { JWT } from "next-auth/jwt"
-import Auth0Provider from "next-auth/providers/auth0"
 import OktaProvider from "next-auth/providers/okta"
+// import Auth0Provider from "next-auth/providers/auth0"
 
 const client = await MongoClient.connect(
 	process.env.DB_CONNECTION_URI as string
@@ -13,6 +13,11 @@ const rolesCollection = db.collection("permissions")
 export const authOptions = {
 	// Configure one or more authentication providers
 	providers: [
+		// Auth0Provider({
+		// 	clientId: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID as string,
+		// 	clientSecret: process.env.AUTH0_CLIENT_SECRET as string,
+		// 	issuer: process.env.NEXT_PUBLIC_AUTH0_ISSUER,
+		// }),
 		OktaProvider({
 			clientId: process.env.NEXT_PUBLIC_OKTA_CLIENT_ID as string,
 			clientSecret: process.env.OKTA_CLIENT_SECRET as string,
