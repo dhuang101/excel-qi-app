3ae9d097eb28c289528556b8cdcbdd538e68d539
Derek Huang <derek.huang@monash.edu>
2026-01-06 16:56:31 +1100

    Add const to rename ards outcomes data correctly

---
+++ src/constants/search/ardsOutcomes.ts
@@ -0,0 +1,19 @@
+// translates the keys to english
+export const ARDS_OUTCOMES = {
+	rrt: "Renal replacement therapy",
+	form11_mech_oxygenator: "Oxygenator failure",
+	form11_mech_pump: "Pump failure",
+	form11_mech_thromb: "Thrombosis/clots: circuit component",
+	form11_haem_surgical: "Surgical site bleeding",
+	form11_haem_major: "Major bleeding",
+	form11_cardio_ami: "Acute myocardial infarction",
+	form11_limb_ischemia: "Limb ischaemia requiring limb reperfusion cannula",
+	form11_mech_problem: "Cannula problems",
+	form11_haem_hemorrhage: "GI haemorrhage",
+	form11_renal_265: "Creatinine >265μmol/L (>3.0)",
+	form11_pulmonary_pule: "Pulmonary embolism",
+	form11_pulmonary_pulh: "Pulmonary haemorrhage",
+	form11_metabolic_heamm: "Moderate haemolysis",
+	form11_limb_pressure: "Pressure injury",
+	form11_neuro_bd: "Brain death",
+}
