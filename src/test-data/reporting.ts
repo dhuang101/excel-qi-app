5b1cf46d469cca0e2ef3aa73c13f79b9dc49eebd
Derek Huang <derek.huang@monash.edu>
2026-01-06 15:27:08 +1100

    Added testing data

---
+++ src/test-data/reporting.ts
@@ -0,0 +1,21 @@
+export const TEST_REPORTING = {
+	ards_outcomes: {
+		form11_mech_oxygenator: 157,
+		form11_mech_pump: 35,
+		form11_mech_thromb: 81,
+		form11_haem_surgical: 272,
+		form11_haem_major: 488,
+		form11_cardio_ami: 15,
+		form11_limb_ischemia: 47,
+		form11_neuro_bd_date_1: 49,
+		rrt: 1442,
+	},
+	imv_duration: {
+		count: 2626.0,
+		min: -242.3472222,
+		q1: 3.1213541662499997,
+		median: 8.9524305555,
+		q3: 19.117534720000002,
+		max: 386.8333333,
+	},
+}
