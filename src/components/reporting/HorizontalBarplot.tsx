f319bc5dd45b2d4592c5ffda854d9722606206e3
Derek Huang <derek.huang@monash.edu>
2026-01-14 09:47:00 +1100

    Moved text wrapping into util function

--- src/components/reporting/HorizontalBarplot.tsx
+++ src/components/reporting/HorizontalBarplot.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from "react"
 import * as d3 from "d3"
+import { SvgWrapText } from "@/utilities/SvgWrapText"
 
 const MARGIN = { top: 30, right: 30, bottom: 70, left: 140 }
 const BAR_PADDING = 0.3
@@ -8,50 +9,6 @@ interface HorizontalBarplotProps {
 	width: number
 	height: number
 	data: { value: string; count: number }[]
-}
-
-const SvgWrapText = (
-	textElement: d3.Selection<SVGTextElement, unknown, null, undefined>,
-	width: number,
-	textContent: string
-): void => {
-	const lineHeight = 1.1
-	const x = textElement.attr("x")
-	const y = textElement.attr("y")
-
-	textElement.text(null)
-	const words = textContent.split(/\s+/).reverse()
-	let word: string | undefined
-	let line: string[] = []
-	let lineNumber = 0
-
-	let tspan = textElement.append("tspan").attr("x", x).attr("y", y)
-
-	while ((word = words.pop())) {
-		line.push(word)
-		tspan.text(line.join(" "))
-		if (
-			(tspan.node()?.getComputedTextLength() ?? 0) > width &&
-			line.length > 1
-		) {
-			line.pop()
-			tspan.text(line.join(" "))
-			line = [word]
-			tspan = textElement
-				.append("tspan")
-				.attr("x", x)
-				.attr("y", y)
-				.text(word)
-			lineNumber++
-		}
-	}
-
-	const totalLines = textElement.selectAll("tspan").size()
-	const bias = (totalLines - 1) / 2
-
-	textElement.selectAll("tspan").attr("dy", (d, i) => {
-		return (i - bias) * lineHeight + "em"
-	})
 }
 
 export const HorizontalBarplot = ({
