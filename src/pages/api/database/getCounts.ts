5cb07310ddfe6d06a3c4e6f8e1be91a6c15982d1
dhuang101 <82797171+dhuang101@users.noreply.github.com>
2026-02-02 14:27:57 +1100

    Add new complications and move them to their own file

--- src/pages/api/database/getCounts.ts
+++ src/pages/api/database/getCounts.ts
@@ -1,28 +1,10 @@
+import { ARDS_COMPLICATIONS_MAP } from "@/constants/reporting/ardsComplications"
 import { MongoClient } from "mongodb"
 import { NextApiRequest, NextApiResponse } from "next"
 
 type ParamsType = {
 	role: string
 	sites: string[]
-}
-
-const ARDS_OUTCOMES_MAP: Record<string, string> = {
-	rrt: "Renal replacement therapy",
-	form11_mech_oxygenator: "Oxygenator failure",
-	form11_mech_pump: "Pump failure",
-	form11_mech_thromb: "Thrombosis/clots: circuit component",
-	form11_haem_surgical: "Surgical site bleeding",
-	form11_haem_major: "Major bleeding",
-	form11_cardio_ami: "Acute myocardial infarction",
-	form11_limb_ischemia: "Limb ischaemia requiring limb reperfusion cannula",
-	form11_mech_problem: "Cannula problems",
-	form11_haem_hemorrhage: "GI haemorrhage",
-	form11_renal_265: "Creatinine >265μmol/L (>3.0)",
-	form11_pulmonary_pule: "Pulmonary embolism",
-	form11_pulmonary_pulh: "Pulmonary haemorrhage",
-	form11_metabolic_heamm: "Moderate haemolysis",
-	form11_limb_pressure: "Pressure injury",
-	form11_neuro_bd: "Brain death",
 }
 
 const uri = process.env.DB_CONNECTION_URI as string
@@ -52,24 +34,7 @@ async function GetCounts(params: ParamsType) {
 	]
 
 	// attributes for ARDS Complications graph
-	const ardsKeys = [
-		"form11_mech_oxygenator",
-		"form11_mech_pump",
-		"form11_mech_thromb",
-		"form11_haem_surgical",
-		"form11_haem_major",
-		"form11_cardio_ami",
-		"form11_limb_ischemia",
-		"form11_mech_problem",
-		"form11_haem_hemorrhage",
-		"form11_renal_265",
-		"form11_pulmonary_pule",
-		"form11_pulmonary_pulh",
-		"form11_metabolic_heamm",
-		"form11_limb_pressure",
-		"form11_neuro_bd",
-		"rrt",
-	]
+	const ardsKeys = Object.keys(ARDS_COMPLICATIONS_MAP)
 
 	const facetStages: Record<string, any[]> = {}
 
@@ -178,7 +143,7 @@ async function GetCounts(params: ParamsType) {
 		if (siteResultsMap[site]) {
 			ardsKeys.forEach((key) => {
 				const count = siteData[key] || 0
-				const displayName = ARDS_OUTCOMES_MAP[key]
+				const displayName = ARDS_COMPLICATIONS_MAP[key]
 
 				siteResultsMap[site].counts.ards_outcomes.push({
 					value: displayName,
