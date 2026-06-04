b979cf0ce5935d770df932c6826bc6eb693e074e
Derek Huang <derek.huang@monash.edu>
2026-06-04 13:44:27 +1000

    Added info card to ecmopal

--- src/pages/ecmo-pal/index.tsx
+++ src/pages/ecmo-pal/index.tsx
@@ -8,9 +8,29 @@ function EcmoPalSearch() {
 		<div className="flex flex-col grow w-full items-center">
 			<div className="w-2/3 h-full">
 				<div className="flex-flex-col w-full">
-					<article className="text-2xl font-semibold my-4">
+					<article className="text-2xl font-semibold mt-4">
 						ECMO PAL Prediction
 					</article>
+					<div className="alert alert-info w-1/2 shadow-sm rounded-lg flex gap-2 items-center text-sm my-2">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							className="h-6 w-6 shrink-0 stroke-current"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+							/>
+						</svg>
+						<article className="">
+							The following pages are for demonstration purposes
+							only. The prediction model is currently incomplete
+							and the data does not represent the EXCEL format.
+						</article>
+					</div>
 					<article className="text-lg font-semibold mb-2">
 						Patient Search
 					</article>
@@ -44,7 +64,7 @@ function EcmoPalSearch() {
 						]}
 						onClick={(event) => {
 							router.push(
-								`/ecmo-pal/${event.currentTarget.dataset.recordId}`
+								`/ecmo-pal/${event.currentTarget.dataset.recordId}`,
 							)
 						}}
 					/>
